7a0376ebdf5407713538ea49fca66e7caa02c5c0
Krishmadhotra <ckrishmadhotra@gmail.com>
2024-07-09 10:03:04 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from "react";
-const App=()=>{
-  return(
-    <h1 className="mt-5 bg-red-800">Hello world!</h1>
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import SignIn from "./pages/SignIn"
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element= {<About />} />
+        <Route path="/dashboard" element= {<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+ 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
