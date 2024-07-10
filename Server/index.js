b797b1f9f69f70a5ea86711fa4df7d79256bd2da
Krishmadhotra <ckrishmadhotra@gmail.com>
2024-07-10 22:24:17 +0530

    backend code

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,34 @@
-import express from 'express'
-import mongoose from "mongoose"
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js';
+import authRoutes from "./routes/auth.route.js"
 
-dotenv.config()
-
-mongoose.connect(process.env.MONGO).then(()=>{
-        console.log("MongoDb is connected");
-    }
-).catch((err)=>{
-    console.log(err);
-})
 const app=express();
-app.listen(3000,()=>{
-    console.log("app is running at");
+dotenv.config();
+app.use(express.json())
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("MongoDB is connected")
 })
+.catch(err=>{
+    console.log(err)
+})
+
+app.listen(3000,()=>{
+    console.log("app is running on port 300 !")
+})
+
+app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes)
+
+app.use((err,req,res,next)=>{
+    const statuscode=err.statuscode || 500;
+    const errMessage=err.message ||'Internal Server Error';
+    res.status(statuscode).json({
+        success:false,
+        statuscode,
+        errMessage
+    })
+
+});
--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose'
 
-const UserSchema=new mongoose.Schema({
+const userSchema= new mongoose.Schema({
     username:{
         type:String,
         required:true,
-        unique:true,
+        unique:true
     },
-
     email:{
         type:String,
         required:true,
         unique:true
     },
-
     password:{
         type:String,
-        required:true,
-    }
-},{timestamps:true}
-)
+        required:true
+    },
+}, {timestamps:true}
+);
 
-const User=mongoose.model("User",UserSchema)
-export default User;
+
+const Users=mongoose.model("Users",userSchema)
+
+export default Users;
