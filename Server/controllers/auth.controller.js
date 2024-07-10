68253fb2782e277dcfb1c2ed2ce1b3bc537099db
Krishmadhotra <ckrishmadhotra@gmail.com>
2024-07-11 02:18:50 +0530

    sign in backend completed

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import Users from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup=async(req,res,next) => {
     const {username,email,password}=req.body;
@@ -23,4 +24,43 @@ export const signup=async(req,res,next) => {
     }catch(error){
         next(error)
     }
+}
+
+export const signin= async  (req,res,next)=>{
+    const{email,password}=req.body;
+
+    if(!email || !password  || email===" " || password===" "){
+      next(errorHandler(400,"Please fill the fields"))
+    }
+
+    try{
+      const validUser=await Users.findOne({email})
+      if(!validUser){
+        return next(errorHandler(400,"User not found"))
+      }
+
+      const validPassword=bcryptjs.compareSync(password,validUser.password);
+      if(!validPassword){
+        return next(errorHandler(400,"Invalid Password"))
+      }
+
+      const token=jwt.sign({
+        id:validUser._id
+      },process.env.JWT_SECRET
+     );
+
+     const {password:pass , ...rest}=validUser._doc
+
+     res.status(200).cookie('access_token',token,{
+      httpOnly:true
+     }).json(rest)
+
+
+
+
+    }
+    catch(error){
+      next(error)
+    }
+
 }
