bcc8c436890481ad80c5b5debb1e22df85ca92ba
Krishmadhotra <ckrishmadhotra@gmail.com>
2024-07-20 14:59:22 +0530

    post page

--- Server/controllers/post.controller.js
+++ Server/controllers/post.controller.js
@@ -70,3 +70,37 @@ export const getposts = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this post'));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('The post has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatePost=async(req,res,next)=>{
+  if(!req.user.isAdmin || req.user.id!==req.params.userID){
+    return next(error(403,"You are not allowed to update this post"))
+  }
+
+  try{
+    const updatedPost=await Post.findByIdAndUpdate(
+      req.paramspostID,
+      {
+        $set:{
+          title:req.body.title,
+          content:req.body.title,
+          image:req.body.image
+        }},{new:true}
+      )
+      res.status(200).json(updatedPost)
+  }catch(error){
+    next(error);
+  }
+
+}
--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import {test,updateUser,deleteUser,signout} from "../controllers/user.controller.js"
-import { verifyToken } from '../utils/verifyUser.js';
+import {test,updateUser,deleteUser,signout,getusers,deleteuser} from "../controllers/user.controller.js"
+import { verifyToken} from '../utils/verifyUser.js';
 
 const router=express.Router();
 
@@ -8,6 +8,7 @@ router.get("/test",test)
 router.put("/update/:userId",verifyToken,updateUser)
 router.delete("/delete/:userId",verifyToken,deleteUser)
 router.post("/signout",signout)
+router.get("/getusers",verifyToken,getusers)
 
 
 export default router;
